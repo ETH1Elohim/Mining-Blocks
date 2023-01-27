3fa136d722300d8962b2a3b39935af133424b8cd
ETH1Elohim <ezmax.defi@gmail.com>
2023-01-27 15:43:04 -0700

    add tx mine

--- Index.js
+++ Index.js
@@ -1,30 +1,35 @@
-const SHA256 = require('crypto-js/sha256');
-const TARGET_DIFFICULTY = BigInt(0x0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
+const SHA256 = require("crypto-js/sha256");
+const TARGET_DIFFICULTY =
+  BigInt(0x0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
 const MAX_TRANSACTIONS = 10;
 
 const mempool = [];
 const blocks = [];
 
 function addTransaction(transaction) {
-    // TODO: add transaction to mempool
-    mempool.push(transaction);
+  // TODO: add transaction to mempool
+  mempool.push(transaction);
 }
 
 function mine() {
-    // TODO: mine a block
-    // stores id of last block
-    const block = { id: blocks.length };
-    // hashes block
-    const hash = SHA256(JSON.stringify(block));
-    // pushes block
-    blocks.push({block, hash});
+  // mine tx:
+  let transactions = [];
+  while (transactions.length < MAX_TRANSACTIONS && mempool.length > 0) {
+    transactions.push(mempool.pop());
+  };
+  // stores id of last block
+  const block = { id: blocks.length };
+  // hashes block
+  const hash = SHA256(JSON.stringify(block));
+  // pushes block
+  blocks.push({ block, hash });
 }
 
 module.exports = {
-    TARGET_DIFFICULTY,
-    MAX_TRANSACTIONS,
-    addTransaction, 
-    mine, 
-    blocks,
-    mempool
+  TARGET_DIFFICULTY,
+  MAX_TRANSACTIONS,
+  addTransaction,
+  mine,
+  blocks,
+  mempool,
 };
